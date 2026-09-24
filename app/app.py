620c4c4d2fa8422953f0f6f2e6643051aad9f610
Harshit Mali <harshitmali930@gmail.com>
2026-09-24 10:06:40 +0530

    Add Kubernetes deployment to CI/CD

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
 def hello():
-    return "Hello from Jenkins CI/CD!"
+    return "Hello from Jenkins CI/CD - Version 2!"
 
 if __name__ == "__main__":
-    print(hello())
+    app.run(host="0.0.0.0", port=5000)
